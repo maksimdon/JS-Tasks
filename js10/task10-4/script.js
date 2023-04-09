50e72248e7e22e15f054e8b2d0146f3fdedb45ad
maksimdon <103524576+maksimdon@users.noreply.github.com>
2023-04-09 11:48:10 +0300

    Update script.js

--- js10/task10-4/script.js
+++ js10/task10-4/script.js
@@ -2,28 +2,49 @@ let submit = document.querySelector('.sub');
 let ul = document.querySelector('ul');
 let text = document.querySelector('.text');
 let form = document.querySelector('form');
-form.addEventListener('submit', (event) =>{
+let tasks = [];
+if(localStorage.tasks){
+    tasks = JSON.parse(localStorage.tasks)
+    tasks.forEach(element =>{
+        let point = document.createElement('li');
+        point.innerText = element;
+        ul.appendChild(point);
+    })
+}
+
+
+// Проверить есть ли что то в localStorage, если есть распарсить и записать в tasks, обойти этот массив и построить li для каждого элемента
+form.addEventListener('submit', (event) => {
     event.preventDefault();
     let li = document.createElement('li');
     let remover = document.createElement('button');
     remover.innerText = 'X'
     li.innerText = (text.value);
+    tasks.push(text.value);
+    localStorage.tasks = JSON.stringify(tasks);
     let checkbox = document.createElement('input');
     checkbox.setAttribute("type", "checkbox");
     li.prepend(checkbox);
-    li.append(remover); 
+    li.append(remover);
     ul.appendChild(li);
     checkbox.addEventListener('click', () => {
-        if (checkbox.checked){
+        if (checkbox.checked) {
             li.classList.add('crossed')
             ul.appendChild(li);
-        } else{
+        } else {
             li.classList.remove('crossed');
             ul.prepend(li);
         }
     });
-    remover.addEventListener('click', ()=>{
+    remover.addEventListener('click', () => {
         li.remove();
     })
-    text.value = ''; 
+    text.value = '';
 });
+let button = document.querySelector('.task2');
+button.addEventListener('click', () => {
+    let li = document.querySelectorAll('li');
+    li.forEach(item => {
+        item.style.textDecoration = 'line-through';
+    })
+})
